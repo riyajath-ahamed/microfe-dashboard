ef2e7b92c962de0fdf185a69dadbfbd7b8c704ef
riyajath-ahamed <riyajatha@gmail.com>
2025-08-05 23:49:42 +0530

    Fedrate | RJ

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Dashboard': './src/dashboard/dashboard.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    })
+  ],
+  build:{
+    target:'exnext',
+    minify: false, // Disable minification for easier debugging
+    cssCodeSplit: false, // Enable CSS code splitting
+  }
 })
